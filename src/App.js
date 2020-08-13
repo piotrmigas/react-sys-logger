7c201fac208c08b70ba886b108c47118d06ad628
piotrmigas <piotr.migas@icloud.com>
2020-08-13 21:44:05 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import SearchBar from "./components/SearchBar";
--- src/redux/actions/logActions.js
+++ src/redux/actions/logActions.js
@@ -1,117 +1,117 @@
-export const getLogs = () => async dispatch => {
+export const getLogs = () => async (dispatch) => {
   try {
-    setLoading();
+    dispatch(setLoading());
     const res = await fetch("/logs");
     const data = await res.json();
     dispatch({
       type: "GET_LOGS",
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: "LOGS_ERROR",
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
-export const searchLogs = text => async dispatch => {
+export const searchLogs = (text) => async (dispatch) => {
   try {
-    setLoading();
+    dispatch(setLoading());
     const res = await fetch(`/logs?q=${text}`);
     const data = await res.json();
     dispatch({
       type: "SEARCH_LOGS",
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: "LOGS_ERROR",
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
 export const setLoading = () => {
   return {
-    type: "SET_LOADING"
+    type: "SET_LOADING",
   };
 };
 
-export const addLog = log => async dispatch => {
+export const addLog = (log) => async (dispatch) => {
   try {
-    setLoading();
+    dispatch(setLoading());
     const res = await fetch("/logs", {
       method: "POST",
       headers: {
-        "content-type": "application/json"
+        "content-type": "application/json",
       },
-      body: JSON.stringify(log)
+      body: JSON.stringify(log),
     });
     const data = await res.json();
     dispatch({
       type: "ADD_LOG",
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: "LOGS_ERROR",
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
-export const deleteLog = id => async dispatch => {
+export const deleteLog = (id) => async (dispatch) => {
   try {
-    setLoading();
+    dispatch(setLoading());
     await fetch(`/logs/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
     });
     dispatch({
       type: "DELETE_LOG",
-      payload: id
+      payload: id,
     });
   } catch (err) {
     dispatch({
       type: "LOGS_ERROR",
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
-export const updateLog = log => async dispatch => {
+export const updateLog = (log) => async (dispatch) => {
   try {
-    setLoading();
+    dispatch(setLoading());
     const res = await fetch(`/logs/${log.id}`, {
       method: "PUT",
       body: JSON.stringify(log),
       headers: {
-        "content-type": "application/json"
-      }
+        "content-type": "application/json",
+      },
     });
     const data = await res.json();
 
     dispatch({
       type: "UPDATE_LOG",
-      payload: data
+      payload: data,
     });
   } catch (err) {
     dispatch({
       type: "LOGS_ERROR",
-      payload: err.response.statusText
+      payload: err.response.statusText,
     });
   }
 };
 
-export const setCurrent = log => {
+export const setCurrent = (log) => {
   return {
     type: "SET_CURRENT",
-    payload: log
+    payload: log,
   };
 };
 
 export const clearCurrent = () => {
   return {
-    type: "CLEAR_CURRENT"
+    type: "CLEAR_CURRENT",
   };
 };
